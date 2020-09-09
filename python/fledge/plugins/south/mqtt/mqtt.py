1b0aabb96038ddf0cd961b0ecb682f22feb03cb5
Praveen Garg <praveen.garg@nerdapplabs.com>
2020-09-09 20:29:42 +0530

    docs

--- python/fledge/plugins/south/mqtt/mqtt.py
+++ python/fledge/plugins/south/mqtt/mqtt.py
@@ -68,7 +68,7 @@ __copyright__ = "Copyright (c) 2020 Dianomic Systems, Inc."
 __license__ = "Apache 2.0"
 __version__ = "${VERSION}"
 
-_LOGGER = logger.setup(__name__, level=logging.DEBUG)
+_LOGGER = logger.setup(__name__, level=logging.INFO)
 
 c_callback = None
 c_ingest_ref = None
